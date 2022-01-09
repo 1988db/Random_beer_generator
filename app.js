f067595157a3102c319d0a69ccbead6eb385feaa
1988db <elozrana@gmail.com>
2022-01-09 23:40:44 +0100

    beers fetch

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', ()=> {
-    console.log('DOMContentLoaded');
+    fetch('https://api.punkapi.com/v2/beers')
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            console.log(data);
+        })
 })
